a8e86bc966797f3e06c47149281f1f13c583f56e
hjf11 <hanajf11@gmail.com>
2024-01-24 00:41:14 +0100

    novi viterbi

--- Viterbi.cpp
+++ Viterbi.cpp
@@ -14,26 +14,35 @@ vector<int> Viterbi(HMM &hmm, Pair pair) {
     vector<vector<double>> previous_states(pair.first.size()-1, vector<double>(NUM_STATES, 1.0));
     //vjerojatnost prvog stanja
     string char_combination = string(1, pair.first[0]) + string(1, pair.second[0]);
-    for(int i = 0; i<NUM_STATES; i++) {
-        max_p[0][i] *= hmm.pi[0][i] * hmm.E[i][char_combination_to_idx[char_combination]];
+    
+    max_p[0][0] *= hmm.pi[0][0] * hmm.E[0][char_combination_to_idx[string(1, '-') + string(1, pair.second[0])]];
+    max_p[0][1] *= hmm.pi[0][1] * hmm.E[1][char_combination_to_idx[string(1, pair.first[0]) + string(1, '-')]];
+    max_p[0][2] *= hmm.pi[0][2] * hmm.E[2][char_combination_to_idx[char_combination]];
 
-    }
     //vjerojatnosti ostalih stanja
     //unaprijedni prolaz
     for(int t = 1; t<pair.first.size(); t++) {
-        char_combination = string(1, pair.first[t]) + string(1, pair.second[t]);
         for(int i = 0; i<NUM_STATES; i++) {
             vector<double> p(NUM_STATES, 1.0);
-            //insertion
-            p[0] *= max_p[t-1][0] * hmm.A[i][0] * hmm.E[0][char_combination_to_idx[
-                string(1, '-') + string(1, pair.second[t])]];
+            switch (i)
+            {
+            case 0:
+                char_combination = string(1, '-') + string(1, pair.second[t]);
+                break;
+            case 1:
+                char_combination = string(1, pair.first[t]) + string(1, '-');
+                break;
+            case 2:
+                char_combination = string(1, pair.first[t]) + string(1, pair.second[t]);
+                break;
+            default:
+                cout << "Error";
+                break;
+            }
 
-            p[1] *= max_p[t-1][1] * hmm.A[i][1] * hmm.E[0][char_combination_to_idx[
-                string(1, pair.first[t]) + string(1, '-')]];
-
-            //match/mismatch
-            p[2] *= max_p[t-1][2] * hmm.A[i][2] * hmm.E[2][char_combination_to_idx[
-                string(1, pair.first[t]) + string(1, pair.second[t])]];
+            for (int j = 0; j<NUM_STATES; j++) {
+                p[j] *= max_p[t-1][j] * hmm.A[j][i] * hmm.E[i][char_combination_to_idx[char_combination]];
+            }
 
             int argmax = get_argmax(p);
             max_p[t][i] = p[argmax];
